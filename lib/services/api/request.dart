0098682956e2e26b8ae732ebb9714381b9fb10ea
kinsprite <qxqs11@126.com>
2021-02-19 22:49:30 +0800

    add dio in dep

---
+++ lib/services/api/request.dart
@@ -0,0 +1,10 @@
+import 'package:dio/dio.dart';
+
+void getHttp() async {
+  try {
+    Response response = await Dio().get("http://www.google.cn");
+    print(response);
+  } catch (e) {
+    print(e);
+  }
+}
